2798243af85a9d6bc53adb3bc4ce2b03c30f23e7
Gabriel Pires De Lima <gabrielhung8@gmail.com>
2024-10-22 13:10:49 -0300

    Concluído

--- tictactoe.py
+++ tictactoe.py
@@ -1,74 +1,97 @@
 import os
-tabuleiro = "_1|_2|_3\n_4|_5|_6\n 7| 8| 9" 
-# aplicar compreensão em dicionário
+
 dict_tabuleiro = [
-    {"piece":1,"played":False},
-    {"piece":2,"played":False},
-    {"piece":3,"played":False},
-    {"piece":4,"played":False},
-    {"piece":5,"played":False},
-    {"piece":6,"played":False},
-    {"piece":7,"played":False},
-    {"piece":8,"played":False},
-    {"piece":9,"played":False},
+    {"piece": i + 1, "played": False, "player": None} 
+    for i in range(9)
 ]
 
+fim = None
 
 def checkFimJogo():
     fim = all(item["played"]==True for item in dict_tabuleiro)
     return fim
     
-def vezJogador(jogada,vezJogador):
+def vezJogador(jogada,vezJogador,jogador):
     if dict_tabuleiro[jogada-1]["played"] == False:
             dict_tabuleiro[jogada-1]["piece"] = vezJogador
             dict_tabuleiro[jogada-1]["played"] = True
+            dict_tabuleiro[jogada-1]["player"] = jogador
     else:
-        print("Este espaço já foi jogado, por favor escolha outro")
+        print("Este espaço já foi jogado, por favor escolha outro!")
 
+    verificaIndices(jogador)
 
-def alteraTabuleiro():
+def checkVitoria(list, player):
+    listPattern = [dict_tabuleiro[list[0]]["player"],dict_tabuleiro[list[1]]["player"], dict_tabuleiro[list[2]]["player"]]
+    checkVic = all(i == player for i in listPattern)
+
+    return checkVic
+
+def verificaIndices(player):
+    global dict_tabuleiro
+    global fim
+    indVictory = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[6,4,2],[0,3,6],[1,4,7],[2,5,8]]
+    for i in range(len(indVictory)):
+        indVic = indVictory[i]
+        victory = checkVitoria(indVic,player)
+        if victory == True:
+            print("Jogador {} ganhou!".format(player))
+            vitoria = True
+            fim = True
+            break
+
+def atualizaTabuleiro():
     global tabuleiro_pecas
-    listFormat = [
-        dict_tabuleiro[0]["piece"],
-        dict_tabuleiro[1]["piece"],
-        dict_tabuleiro[2]["piece"],
-        dict_tabuleiro[3]["piece"],
-        dict_tabuleiro[4]["piece"],
-        dict_tabuleiro[5]["piece"],
-        dict_tabuleiro[6]["piece"],
-        dict_tabuleiro[7]["piece"],
-        dict_tabuleiro[8]["piece"]
-    ]
+    listFormat = [dict_tabuleiro[i]["piece"] for i in range(len(dict_tabuleiro))]
 
     tabuleiro_pecas = "_{}|_{}|_{}\n_{}|_{}|_{}\n {}| {}| {}".format(*listFormat)
 
-alteraTabuleiro()
-
-print(tabuleiro_pecas)
+    print(tabuleiro_pecas)
 
 def jogando():
+    print("Bem vindo ao jogo da velha!")
+
+    vezJogada = None
+    choicePlayer1 = int(input("Jogador 1, por favor escolha sua peça! \n 1 - X  2 - o :"))
+    if choicePlayer1 == 1:
+        vezJogada = "X"
+    else:
+        vezJogada = "o"
+
+    print("Você escolheu a peça {}!".format(vezJogada))
+
+    atualizaTabuleiro()
     global dict_tabuleiro
 
     checkFim = False
-    vezJogada = "X"    
-    while checkFim==False:
+    jogador = 1
 
+    while not checkFim:
+        
+        print("Jogador {}!".format(jogador))
         jogada = int(input(" Insira sua jogada:  "))
         os.system('cls') or None
         global tabuleiro_pecas
         
-        vezJogador(jogada,vezJogada)
+        vezJogador(jogada,vezJogada, jogador)
 
-        if vezJogada == "O":
+        if vezJogada == "o":
             vezJogada = "X"
         else:
-            vezJogada = "O"
+            vezJogada = "o"
         
-        alteraTabuleiro()
-        #tabuleiro = tabuleiro.replace("{}".format(jogada),"O")
+        if jogador == 1:
+            jogador = 2
+        else:
+            jogador = 1 
 
-        print(tabuleiro_pecas)
+        atualizaTabuleiro()
+        
         checkFim = checkFimJogo()
-
+        verificaIndices(jogador)
+        if fim == True:
+            checkFim = True
+            
+    print("Fim de jogo!")
 
 jogando()
